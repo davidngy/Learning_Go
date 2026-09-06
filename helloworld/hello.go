007c35138a2518a4424b54057da4522d668f10e6
david <davidngy@gmail.com>
2026-09-06 15:17:20 +0200

    add italian to languages

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,16 +1,20 @@
 package main
 
-import "fmt"
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+)
 
-const englishHelloPrefix = "Hello, "
-
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	// fmt.Println(Hello("world"))
 }
